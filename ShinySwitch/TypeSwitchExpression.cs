96410bf8b96fa7ddee0defbba29e521b242ee7ce
Asger Hallas <ahl@asgerhallas.dk>
2016-10-11 09:15:48 +0200

    predicates on multi matches

--- ShinySwitch/TypeSwitchExpression.cs
+++ ShinySwitch/TypeSwitchExpression.cs
@@ -62,6 +62,24 @@ namespace ShinySwitch
         public TypeSwitchExpression2<TLeft, TRight, TExpression> MatchRight<TR>(Func<TLeft, TR, TExpression> func) where TR : TRight =>
             MatchIf(subject.Item2 is TR, () => func(subject.Item1, (TR)subject.Item2));
 
+        public TypeSwitchExpression2<TLeft, TRight, TExpression> Match<TL, TR>(Func<TL, TR, bool> predicate, Func<TL, TR, TExpression> func) =>
+            MatchIf(subject.Item1 is TL && subject.Item2 is TR && predicate((TL)(object)subject.Item1, (TR)(object)subject.Item2), () => func((TL)(object) subject.Item1, (TR)(object) subject.Item2));
+
+        public TypeSwitchExpression2<TLeft, TRight, TExpression> Match(TLeft left, TRight right, Func<bool> predicate, Func<TLeft, TRight, TExpression> func) =>
+            MatchIf(Equals(subject.Item1, left) && Equals(subject.Item2, right) && predicate(), () => func(subject.Item1, subject.Item2));
+
+        public TypeSwitchExpression2<TLeft, TRight, TExpression> MatchLeft<TL>(Func<TL, bool> predicate, Func<TL, TRight, TExpression> func) where TL : TLeft =>
+            MatchIf(subject.Item1 is TL && predicate((TL)subject.Item1), () => func((TL)subject.Item1, subject.Item2));
+
+        public TypeSwitchExpression2<TLeft, TRight, TExpression> MatchLeft<TL>(TRight right, Func<TL, bool> predicate, Func<TL, TRight, TExpression> func) where TL : TLeft  =>
+            MatchIf(subject.Item1 is TL && Equals(subject.Item2, right) && predicate((TL)subject.Item1), () => func((TL)subject.Item1, subject.Item2));
+
+        public TypeSwitchExpression2<TLeft, TRight, TExpression> MatchRight<TR>(TLeft left, Func<TR, bool> predicate, Func<TLeft, TR, TExpression> func) where TR : TRight =>
+            MatchIf(Equals(subject.Item1, left) && subject.Item2 is TR && predicate((TR)subject.Item2), () => func(subject.Item1, (TR)subject.Item2));
+
+        public TypeSwitchExpression2<TLeft, TRight, TExpression> MatchRight<TR>(Func<TR, bool> predicate, Func<TLeft, TR, TExpression> func) where TR : TRight =>
+            MatchIf(subject.Item2 is TR && predicate((TR)subject.Item2), () => func(subject.Item1, (TR)subject.Item2));
+
         public TypeSwitchExpression2<TLeft, TRight, TExpression> Then(Func<TExpression, TLeft, TRight, TExpression> func) =>
             result.HasResult
                 ? new TypeSwitchExpression2<TLeft, TRight, TExpression>(
