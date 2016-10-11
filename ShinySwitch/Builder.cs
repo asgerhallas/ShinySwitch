e53f723bf0bf5b2be1453abbe9e9b6a661c8afeb
Asger Hallas <ahl@asgerhallas.dk>
2016-10-11 09:09:56 +0200

    lazy

--- ShinySwitch/Builder.cs
+++ ShinySwitch/Builder.cs
@@ -7,10 +7,89 @@ namespace ShinySwitch
     //the match method it self could also check - but that would only be ok if the switch is cached eg lazy... is it a good idea, or should it be kept simpler?
 
     //todo: make it possible to seperate build of matches and execution, so it could be intercepted to build eg a visitor where steps can be intercepted
-    //it could also be intercepted when eager ... hmm..
+    //it could also be intercepted when eager ... hmm.. yes, but I can not send a new value into the same switch again... could i do that otherwise?
 
     //if lazy then either build _and_ execution is done with extension methods, for same syntax as now (to be able to give value upfront and carry it along until execution)
     //or a build that you pass to the executor like: Switch<string>.On(value, Switch.Match<>().Else(x)) and behind the scenes: Switch.Match<>().Else(x).Execute(value)
+
+    public static class Switch2<TExpression>
+    {
+        public static Builder<object, TExpression> On() => On<object>();
+
+        public static Builder<TSubject, TExpression> On<TSubject>()
+        {
+            return new Builder<TSubject, TExpression>();
+        }
+    }
+
+    public class Test
+    {
+        public void noget()
+        {
+            Switch2<string>.On()
+                .Match<string>(x => "")
+                .Else(() => "")
+                .Execute();
+        }
+    }
+
+    public class SwitchExpression2<TSubject, TExpression>
+    {
+        Func<SwitchResult<TExpression>, TSubject, bool> predicate;
+        Func<TSubject, TExpression> result;
+
+        public SwitchExpression2(SwitchExpression2<TSubject, TExpression> tail, Func<SwitchResult<TExpression>, TSubject, bool> predicate, Func<TSubject, TExpression> result)
+        {
+            this.predicate = predicate;
+            this.result = result;
+        }
+
+        public bool IsEmpty => false;
+        public SwitchExpression2<TSubject, TExpression> Tail { get; }
+
+        public TExpression Evaluate(TSubject subject)
+        {
+            return null;
+        }
+
+        public SwitchExpression2<TSubject, TExpression> Append(Func<SwitchResult<TExpression>, TSubject, bool> predicate, Func<TSubject, TExpression> result)
+        {
+            return new SwitchExpression2<TSubject, TExpression>(this, predicate, result);
+        }
+
+        internal SwitchExpression2<TSubject, TExpression> Reverse()
+        {
+            var reverse = new SwitchExpression2<TSubject, TExpression>(null, predicate, result);
+
+            for (SwitchExpression2<TSubject, TExpression> x = Tail; x.Tail != null; x = x.Tail)
+                reverse = reverse.Append(x.predicate, x.result);
+
+            return reverse;
+        }
+    }
+
+
+    public class Builder<TSubject, TExpression>
+    {
+        readonly SwitchExpression2<TSubject, TExpression> expression;
+
+        internal Builder()
+        {
+        }
+
+        internal Builder(SwitchExpression2<TSubject, TExpression> expression)
+        {
+            this.expression = expression;
+        }
+
+        public Builder<TSubject, TExpression> Match<T>(Func<T, TExpression> func) =>
+            New(expression.Append((result, subject) => subject is T, subject => func((T) (object) subject)));
+
+        public SwitchExpression2<TSubject, TExpression> Else(Func<TExpression> func) =>
+            expression.Append((result, subject) => !result.HasResult, subject => func()).Reverse();
+
+        Builder<TSubject, TExpression> New(SwitchExpression2<TSubject, TExpression> ex) => new Builder<TSubject, TExpression>(ex);
+    }
 
     public class TypeSwitchExpression<TSubject, TExpression> : SwitchExpression<TSubject, TExpression>
     {
